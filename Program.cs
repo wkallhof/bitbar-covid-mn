f3616d7b4aa5c4771996b7935eeb7e979a8bc1d2
Wade Kallhoff <wadekallhoff@Wades-MacBook-Pro.local>
2020-03-25 15:36:17 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Covid
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            var client = new HttpClient();
+            var result = await client.GetAsync("https://www.health.state.mn.us/diseases/coronavirus/situation.html");
+            result.EnsureSuccessStatusCode();
+
+            var html = await result.Content.ReadAsStringAsync();
+
+            Console.Out.WriteLine($"Positive: {GetPositiveCount(html)}");
+
+            Console.Out.WriteLine($"---");
+
+            Console.Out.WriteLine($"Deaths: {GetDeathCount(html)}");
+            Console.Out.WriteLine($"Last Updated: {GetLastUpdated(html)}");
+
+            Console.Out.WriteLine($"---");
+            Console.Out.WriteLine($"MN DOH | href=https://www.health.state.mn.us/diseases/coronavirus/situation.html");
+
+            return 1;
+        }
+
+        static int GetPositiveCount(string html){
+            var match = Regex.Match(html,"<li><strong>Positive:</strong> (.*)</li>");
+            if(!match.Success || match.Groups.Count != 2)
+                return 0;
+
+            return int.Parse(match.Groups[1].Value);
+        }
+
+        static int GetDeathCount(string html){
+            var match = Regex.Match(html,"<li><strong>Deaths:</strong> (.*)</li>");
+            if(!match.Success || match.Groups.Count != 2)
+                return 0;
+
+            return int.Parse(match.Groups[1].Value);
+        }
+
+        static string GetLastUpdated(string html){
+            var match = Regex.Match(html,"<p class=\"small\">Updated (.*)</p>");
+            if(!match.Success || match.Groups.Count != 2)
+                return "N/A";
+
+            return match.Groups[1].Value;
+        }
+    }
+}
---
+++ covid.10m.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/local/share/dotnet/dotnet ~/Documents/Bitbar\ Plugins/covid/Covid.dll
